4c701a47b5ef41bd23d6e53a9951d0a172c1faa8
oryjk <oryjk@qq.com>
2024-10-26 22:23:00 +0800

    Implement user team unbinding and load server port from .env
    Added the unbind_user_team function to handle unbinding requests and updated the user routes to include bind and unbind endpoints. Enhanced main.rs to load environment variables, specifically the server port, from a .env file. Updated .env with DATABASE_URL format and added SERVER_PORT configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
 
 mod db;
 mod handlers;
@@ -7,6 +9,12 @@ mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // 加载环境变量
+    dotenv().ok();
+    let server_port = env::var("SERVER_PORT")
+        .expect("无法获取到端口环境变量，请设置")
+        .parse::<u16>()
+        .expect("端口环境变量不是有效的u16数字");
     let pool = db::connections::create_pool()
         .await
         .expect("Failed to create pool");
@@ -16,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(pool.clone()))
             .configure(routes::user::user_routes)
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", server_port))?
         .run()
         .await
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,9 +1,10 @@
-use crate::handlers::user_team_handler::bind_user_team;
+use crate::handlers::user_team_handler::{bind_user_team, unbind_user_team};
 use actix_web::web;
 
 pub fn user_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/users")
-            .route("/teams/{user_id}/{team_id}", web::get().to(bind_user_team)),
+            .route("/teams/bind/{user_id}/{team_id}", web::post().to(bind_user_team))
+            .route("/teams/unbind/{user_id}/{team_id}", web::post().to(unbind_user_team))
     );
 }
